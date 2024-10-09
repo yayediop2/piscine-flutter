a56c116c8ff577b8edfe148a5a00e66c6d9fab1e
yayediop <mamyta2000@outlook.fr>
2024-10-09 02:13:57 +0000

    add new one

--- named-optional-sum.dart
+++ named-optional-sum.dart
@@ -1,3 +1,13 @@
 int namedOptionalSum({int first = 0, int second = 0, int third = 0}) {
   return first + second + third;
 }
+
+// void main() {
+//   int sum1 = namedOptionalSum(first: 5, second: 10);
+//   print(sum1);
+//   int sum2 = namedOptionalSum(); // Omitting all
+//   print(sum2); // Output: 0
+
+//   int sum3 = namedOptionalSum(first: 7, third: 3);
+//   print(sum3);
+// }
---
+++ optional-sum.dart
@@ -0,0 +1,3 @@
+int optionalSum(int first, int second,{ int third = 0}) {
+  return first + second + third;
+}
