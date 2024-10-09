96cf0586b58f57c60eace73157c7113b8c697110
yayediop <mamyta2000@outlook.fr>
2024-10-09 23:43:47 +0000

    update

--- student.dart
+++ student.dart
@@ -3,6 +3,7 @@ import 'university.dart';
 class Student extends University {
   int batch;
   int level;
-  String _secretKey = '01' ;
-  Student(this.batch, this.level, this._secretKey, super._name, super._city, super._ranking);
-}
+  String _secretKey = '01';
+ Student(this.batch, this.level, this._secretKey, String name, String city, int? ranking)
+      : super(name, city, ranking);
+      }
