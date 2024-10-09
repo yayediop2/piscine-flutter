7821f7d8327d7299a5aeef323063485fd3c1d2b6
yayediop <mamyta2000@outlook.fr>
2024-10-09 01:44:44 +0000

    add new one

---
+++ data-structures.dart
@@ -0,0 +1,11 @@
+List<int> listNum = [1, 2, 3, 4, 5, 5];
+List<Object> listObj = ['gigi', 23, 'amam', 'lili', 'wawa'];
+List<String> listStr = ['gigi', 'LALA', 'amam', 'lili', 'wawa'];
+List<List<Object>> listList = [listObj, listStr, listNum];
+Set<String> setStr = {'gigi', 'vivi', 'amam', 'lili', 'wawa'};
+Map<String, int> mapStr = {
+  'ananas' : 2,
+  'banane' : 4,
+  'poire' : 1,
+  'papaye' : 5,
+};
