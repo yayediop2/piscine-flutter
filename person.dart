1f6f131cd6844dfec77826ea2c7c36e53bc5cc3d
yayediop <mamyta2000@outlook.fr>
2024-10-09 18:16:17 +0000

    update new one

--- person.dart
+++ person.dart
@@ -1,16 +1,9 @@
 class Person {
-  String name;                // Name must be a String
-  String surname;             // Surname must be a String
-  String cityOfOrigin;        // City of Origin must be a String
-  int age;                   // Age must be an int
-  int height;                // Height must be an int
+  String name;
+  String surname;
+  String cityOfOrigin;
+  int age;
+  int height;
 
-  // Constructor accepting parameters in the correct order
-  Person(
-    this.name,                // Required
-    this.surname,             // Required
-    this.cityOfOrigin,        // Required
-    this.height,              // Required
-    [this.age = 15]           // Optional with a default value
-  );
+  Person(this.name, this.cityOfOrigin, this.height, {this.age = 10, this.surname = 'lolo'});
 }
