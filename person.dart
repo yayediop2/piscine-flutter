136bcc078251edc195f139629f750bb09740eda0
yayediop <mamyta2000@outlook.fr>
2024-10-09 18:32:44 +0000

    update new one

--- person.dart
+++ person.dart
@@ -1,15 +1,9 @@
 class Person {
   String name;
-  String? surname;
+  String surname;
   String cityOfOrigin;
   int age;
   int height;
 
-  Person({
-    required this.name,
-    required this.cityOfOrigin,
-    required this.age,
-    required this.height,
-    this.surname,
-  });
+Person(this.name, this.cityOfOrigin, this.age, this.height, [this.surname]);
 }
