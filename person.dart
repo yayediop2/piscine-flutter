5a981a1624a5db8eccec9a9d1ecc4891f9b197b4
yayediop <mamyta2000@outlook.fr>
2024-10-09 02:30:35 +0000

    update new one

---
+++ person.dart
@@ -0,0 +1,15 @@
+class Person {
+  String name;
+  String surname;
+  String cityOfOrigin;
+  int age;
+  int height;
+
+  Person({
+    required this.name,
+    required this.surname,
+    required this.cityOfOrigin,
+    required this.age,
+    required this.height,
+  });
+}
